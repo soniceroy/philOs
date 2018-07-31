a228464e89e25f775bccc98b9bca470f8e6fdf57
Matthew Foreman <soniceroy@gmail.com>
2018-07-31 15:17:36 -0500

    minimal rust kernel

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 https://os.phil-opp.com/
-To build, use 'cargo rustc -- -Z pre-link-arg=-nostartfiles'
+
+System prereqs: 
+  'cargo install cargo-xbuild'
+  'rustup component add rust-src'
+  'cargo install bootimage --version "^0.5.0"'
+  
+To build: 'bootimage build'
+To run (using qemu): 'bootimage run'
+
+To write to usb stick for use on a real machine (assuming in project directory):
+    where sdX is the device name:
+        'dd if=target/x86_64-phil_os/debug/bootimage.bin of=/dev/sdX && sync'
+    
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,31 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+extern crate bootloader_precompiled;
 
 // C runtime zero (crt0) and start
 // are overwritten and our own
 // _start is written for a new
 // entrypoint
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
-pub extern "C" fn _start() -> ! { // never type "!"
-    loop {}                       // because it never returns
+pub extern "C" fn _start() -> ! { // "!" is of the "never" type
+                                  // because it never returns
+
+    // raw pointer cast to the vga buffer address 0xb8000
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // set the char in the VGA buffer to the i-ith HELLO char
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // set the corresponding color to 0xb (light cyan)
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
 
 /// This function is called on panic
